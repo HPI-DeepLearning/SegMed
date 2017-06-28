91f81c2f7f6e77ccda27265627ce525744986f66
Highlife1911 <Highlife1911@github.com>
2017-06-28 09:27:29 +0200

    Change parameters for 7D training.

--- pix2pix-ultimate/train-x.py
+++ pix2pix-ultimate/train-x.py
@@ -2,11 +2,11 @@ from shared import *
 
 class pix():
     axis = 'x'
-    dataset_name = 'Combined5'
+    dataset_name = 'Png-HGG7b'
     input_c_dim = 4
-    output_c_dim = 1
+    output_c_dim = 3
     speed_factor = 2
-    batch_size = 32
+    batch_size = 16
     gf_dim = 64
     df_dim = 64
     phase = 'train'
