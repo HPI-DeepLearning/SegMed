84f59b17f439b9f3b9bfa2e0623bcd4a44b291e0
martin fischer <martin.fis@gmx.net>
2017-07-04 22:49:13 +0200

    implement hausdorff distance

--- pix2pix-ultimate/metrics.py
+++ pix2pix-ultimate/metrics.py
@@ -1,5 +1,8 @@
 import numpy as np
 import scipy.misc
+from scipy.ndimage import _ni_support
+from scipy.ndimage.morphology import distance_transform_edt, binary_erosion,\
+    generate_binary_structure
 
 from utils import *
 
@@ -37,9 +40,42 @@ def dice_score(gt_image, predicted_image, threshold_value=128.0):
            "dice score"
 
 
-def hausdorf_distance(gt_image, predicted_image):
-    #TODO
-    return 0
+def hausdorff_distance(gt_image, predicted_image):
+    """
+    Hausdorff Distance. http://pythonhosted.org/MedPy/_modules/medpy/metric/binary.html
+
+    Computes the (symmetric) Hausdorff Distance (HD) between the binary objects in two
+    images. It is defined as the maximum surface distance between the objects.
+
+    Parameters
+    ----------
+    input1 : array_like
+        Input data containing objects. Can be any type but will be converted
+        into binary: background where 0, object everywhere else.
+    input2 : array_like
+        Input data containing objects. Can be any type but will be converted
+        into binary: background where 0, object everywhere else.
+    voxelspacing : float or sequence of floats, optional
+        The voxelspacing in a distance unit i.e. spacing of elements
+        along each dimension. If a sequence, must be of length equal to
+        the input rank; if a single number, this is used for all axes. If
+        not specified, a grid spacing of unity is implied.
+    connectivity : int
+        The neighbourhood/connectivity considered when determining the surface
+        of the binary objects. This value is passed to
+        `scipy.ndimage.morphology.generate_binary_structure` and should usually be :math:`> 1`.
+        Presumably does not influence the result in the case of the Hausdorff distance.
+
+    Returns
+    -------
+    hd : float
+        The symmetric Hausdorff Distance between the object(s) in ```input1``` and the
+        object(s) in ```input2```. The distance unit is the same as for the spacing of
+        elements along each dimension, which is usually given in mm.
+    """
+    hd1 = __surface_distances(gt_image, predicted_image, None, 1).max()
+    hd2 = __surface_distances(predicted_image, gt_image, None, 1).max()
+    return max(hd1, hd2), "hausdorff distance"
 
 
 def sensitivity(gt_image, predicted_image, threshold_value=128.0):
@@ -116,13 +152,13 @@ def prepare_images(file, batch_size=16, channels=10):
     return output
 
 
-def execute_metrics(images, metrics=[example_metric],channels=3 ,source_offset=0, predicted_offset=7):
+def execute_metrics(images, metrics=[example_metric], channels=3, gt_offset=0, predicted_offset=7):
     """ Execution functions to apply multiple metric functions on images.
 
     :param images: array - row of images to apply metric functions on
     :param metrics: array - metric functions to apply
     :param channels: integer - amount of channels to compare
-    :param source_offset - offset in the image row for the ground truth images
+    :param gt_offset - offset in the image row for the ground truth images
     :param predicted_offset - offset in the image row for the target images
     :return: tuple(array, array) - computed metric values (slices(rows), metrics, tumor regions), name of the metric
     """
@@ -130,7 +166,7 @@ def execute_metrics(images, metrics=[example_metric],channels=3 ,source_offset=0
     results = np.empty((images.shape[0], len(metrics), channels))
     metric_names = {}
     for i, row in enumerate(images):
-        gt = row[source_offset:channels]
+        gt = row[gt_offset:channels]
         predicted = row[predicted_offset:predicted_offset + channels]
         for j, metric in enumerate(metrics):
             for k, gt_image, predicted_image in zip(range(3), gt, predicted):
@@ -139,9 +175,44 @@ def execute_metrics(images, metrics=[example_metric],channels=3 ,source_offset=0
     return results, metric_names
 
 
+def __surface_distances(input1, input2, voxelspacing=None, connectivity=1.0):
+    """
+    http://pythonhosted.org/MedPy/_modules/medpy/metric/binary.html
+    The distances between the surface voxel of binary objects in input1 and their
+    nearest partner surface voxel of a binary object in input2.
+    """
+    input1 = np.atleast_1d(input1.astype(np.bool))
+    input2 = np.atleast_1d(input2.astype(np.bool))
+    if voxelspacing is not None:
+        voxelspacing = _ni_support._normalize_sequence(voxelspacing, input1.ndim)
+        voxelspacing = np.asarray(voxelspacing, dtype=np.float64)
+        if not voxelspacing.flags.contiguous:
+            voxelspacing = voxelspacing.copy()
+
+    # binary structure
+    footprint = generate_binary_structure(input1.ndim, connectivity)
+
+    # test for emptiness
+    if 0 == np.count_nonzero(input1):
+        return np.array([float("inf")])  # Can´t calculate the hausdorff distance if there is no object
+    if 0 == np.count_nonzero(input2):
+        return np.array([float("inf")])  # Can´t calculate the hausdorff distance if there is no object
+
+        # extract only 1-pixel border line of objects
+    input1_border = input1 ^ binary_erosion(input1, structure=footprint, iterations=1)
+    input2_border = input2 ^ binary_erosion(input2, structure=footprint, iterations=1)
+
+    # compute average surface distance
+    # Note: scipys distance transform is calculated only inside the borders of the
+    #       foreground objects, therefore the input has to be reversed
+    dt = distance_transform_edt(~input2_border, sampling=voxelspacing)
+    sds = dt[input1_border]
+
+    return sds
+
+
 if __name__ == '__main__':
     images = prepare_images(file='test-x/test_0118.png')
-    metrics = execute_metrics(images, [dice_score, specificity, sensitivity])
+    metrics = execute_metrics(images, [dice_score, specificity, sensitivity, hausdorff_distance])
     # slices(rows) x metrics x 3(tumor regions)
     print(metrics)
-
