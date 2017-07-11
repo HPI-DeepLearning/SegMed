76bd13ca94b70782447c75792d5475591b82219c
Highlife1911 <Highlife1911@github.com>
2017-07-11 14:55:01 +0200

    Implement contest mode.

--- pix2pix-ultimate/train-x.py
+++ pix2pix-ultimate/train-x.py
@@ -5,12 +5,13 @@ class pix():
     dataset_name = 'Png-HGG7b'
     input_c_dim = 4
     output_c_dim = 3
-    speed_factor = 2
-    batch_size = 16
-    gf_dim = 64
-    df_dim = 64
+    speed_factor = 1
+    batch_size = 24
+    gf_dim = 128
+    df_dim = 128
+    pix.lr = 0.0002
 	# phase is train, test or contest
-    phase = 'test'
+    phase = 'train'
     
 var = pix()
 init(var)
